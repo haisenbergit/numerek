4e003aea3b7573326d169610a7da0813a93690af
luk <luk@email.com>
2025-11-03 12:51:14 +0100

    feat: implement user avatar component with current user data and sign-out functionality

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -11,6 +11,7 @@
 import type * as ResendOTP from "../ResendOTP.js";
 import type * as auth from "../auth.js";
 import type * as http from "../http.js";
+import type * as users from "../users.js";
 
 import type {
   ApiFromModules,
@@ -30,6 +31,7 @@ declare const fullApi: ApiFromModules<{
   ResendOTP: typeof ResendOTP;
   auth: typeof auth;
   http: typeof http;
+  users: typeof users;
 }>;
 declare const fullApiWithMounts: typeof fullApi;
 
--- src/features/auth/components/user-avatar.tsx
+++ src/features/auth/components/user-avatar.tsx
@@ -7,18 +7,38 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { UseCurrentUser } from "@/features/auth/hooks/use-current-user";
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export const UserAvatar = () => {
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = UseCurrentUser();
+
+  if (isLoading)
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+
+  if (!data) return null;
+
+  const { image, name, email } = data;
+
+  const avatarFallback = email!.slice(0, 2).toUpperCase();
+
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="relative outline-none">
         <Avatar className="size-10 transition hover:opacity-75">
-          <AvatarImage src="" alt="User avatar" />
-          <AvatarFallback>U</AvatarFallback>
+          <AvatarImage src={image} alt={name} />
+          <AvatarFallback className="bg-green-600 font-bold text-white">
+            {avatarFallback}
+          </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="center" side="right" className="w-60">
-        <DropdownMenuItem></DropdownMenuItem>
+      <DropdownMenuContent align="center" side="bottom" className="w-30">
+        <DropdownMenuItem onClick={() => signOut()}>
+          <LogOut className="mr-2 size-4" />
+          Wyloguj mnie
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
