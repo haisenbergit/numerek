a601c17f814f8aa271b80e4b6362bfd0f4267a06
luk <luk@email.com>
2025-11-10 15:26:11 +0100

    feat: enhance toolbar with workspace data and search functionality

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -1,14 +1,28 @@
+import { Info, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 export const Toolbar = () => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+
   return (
     <nav className="flex h-10 items-center justify-between bg-[#481349] p-1.5">
       <div className="flex-1" />
-      <div className="max-[642px] min-w-[280px] shrink grow-[2]">
+      <div className="max-[642px] flex min-w-[280px] shrink grow-[2] items-center">
         <Button
           size="sm"
           className="h-7 w-full justify-start bg-accent/25 px-2 hover:bg-accent/25"
-        ></Button>
+        >
+          <Search className="mr-2 size-4 text-white" />
+          <span className="text-xs text-white">Search {data?.name}</span>
+        </Button>
+      </div>
+      <div className="ml-auto flex flex-1 items-center justify-end">
+        <Button variant="transparent" size="iconSm">
+          <Info className="size-5 text-white" />
+        </Button>
       </div>
     </nav>
   );
