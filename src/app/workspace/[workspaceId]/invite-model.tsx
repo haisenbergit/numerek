d7eb99535f9322045b7c74bf4b80f7da2f9ee9b8
luk <luk@email.com>
2025-11-30 12:18:39 +0100

    feat: enhance InviteModal with dynamic workspace name and join code display

--- src/app/workspace/[workspaceId]/invite-model.tsx
+++ src/app/workspace/[workspaceId]/invite-model.tsx
@@ -1,6 +1,9 @@
+import { CopyIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -8,15 +11,34 @@ import {
 interface InviteModalProps {
   open: boolean;
   setOpen: (open: boolean) => void;
+  name: string;
+  joinCode: string;
 }
 
-export const InviteModal = ({ open, setOpen }: InviteModalProps) => {
+export const InviteModal = ({
+  open,
+  setOpen,
+  name,
+  joinCode,
+}: InviteModalProps) => {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Invite people to your workspace</DialogTitle>
+          <DialogTitle>Invite people to {name}</DialogTitle>
+          <DialogDescription>
+            Use the code below to invite people to your workspace
+          </DialogDescription>
         </DialogHeader>
+        <div className="flex flex-col items-center justify-center gap-y-4 py-10">
+          <p className="text-4xl font-bold uppercase tracking-widest">
+            {joinCode}
+          </p>
+          <Button variant="ghost" size="sm">
+            Copy link
+            <CopyIcon className="ml-2 size-4" />
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
