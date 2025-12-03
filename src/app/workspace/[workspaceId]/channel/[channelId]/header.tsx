5f279d0b23a9132cef2eee9c0a51e9404d9b5953
luk <luk@email.com>
2025-12-03 23:48:53 +0100

    feat: add dialog component for channel header with edit option

--- src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/header.tsx
@@ -1,5 +1,12 @@
 import { FaChevronDown } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 interface HeaderProps {
   title: string;
@@ -8,14 +15,34 @@ interface HeaderProps {
 export const Header = ({ title }: HeaderProps) => {
   return (
     <div className="flex h-[49px] items-center overflow-hidden border-b bg-white px-4">
-      <Button
-        variant="ghost"
-        className="w-auto overflow-hidden px-2 text-lg font-semibold"
-        size="sm"
-      >
-        <span className="truncate"># {title}</span>
-        <FaChevronDown className="ml-2 size-2.5" />
-      </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button
+            variant="ghost"
+            className="w-auto overflow-hidden px-2 text-lg font-semibold"
+            size="sm"
+          >
+            <span className="truncate"># {title}</span>
+            <FaChevronDown className="ml-2 size-2.5" />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="overflow-hidden bg-gray-50 p-0">
+          <DialogHeader className="border-b bg-white p-4">
+            <DialogTitle># {title}</DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-2 px-4 pb-4">
+            <div className="cursor-pointer rounded-lg border bg-white px-5 py-4 hover:bg-gray-50">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-semibold">Channel name</p>
+                <p className="text-sm font-semibold text-[#1264a3] hover:underline">
+                  Edit
+                </p>
+              </div>
+              <p className="text-sm"># {title}</p>
+            </div>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
