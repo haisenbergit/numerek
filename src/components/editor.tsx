28a3b463a1468f614346fed250c73df0749ba65e
luk <luk@email.com>
2025-12-07 23:18:14 +0100

    feat: integrate Quill editor component into the editor

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,8 +1,33 @@
+import { useEffect, useRef } from "react";
+import Quill, { type QuillOptions } from "quill";
+import "quill/dist/quill.snow.css";
+
 const Editor = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const container = containerRef.current;
+    const editorContainer = container.appendChild(
+      container.ownerDocument.createElement("div")
+    );
+
+    const options: QuillOptions = {
+      theme: "snow",
+    };
+
+    new Quill(editorContainer, options);
+
+    return () => {
+      if (container) container.innerHTML = "";
+    };
+  }, []);
+
   return (
     <div className="flex flex-col">
       <div className="transit flex flex-col overflow-hidden rounded-md border border-slate-200 focus-within:border-slate-300 focus-within:shadow-sm">
-        <div>Editor</div>
+        <div ref={containerRef} />
       </div>
     </div>
   );
