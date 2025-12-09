3c5297ebd2cb91ff8fc1829aa12cbb553cb72410
luk <luk@email.com>
2025-12-09 23:45:51 +0100

    feat: implement dynamic loading for editor component and enhance chat input with action buttons

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useRef } from "react";
+import { ImageIcon, Smile } from "lucide-react";
 import Quill, { type QuillOptions } from "quill";
 import "quill/dist/quill.snow.css";
+import { MdSend } from "react-icons/md";
+import { PiTextAa } from "react-icons/pi";
+import { Button } from "@/components/ui/button";
 
 const Editor = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -28,6 +32,40 @@ const Editor = () => {
     <div className="flex flex-col">
       <div className="transit flex flex-col overflow-hidden rounded-md border border-slate-200 focus-within:border-slate-300 focus-within:shadow-sm">
         <div ref={containerRef} className="ql-custom h-full" />
+        <div className="z-[5] flex px-2 pb-2">
+          <Button
+            disabled={false}
+            size="iconSm"
+            variant="ghost"
+            onClick={() => {}}
+          >
+            <PiTextAa className="size-4" />
+          </Button>
+          <Button
+            disabled={false}
+            size="iconSm"
+            variant="ghost"
+            onClick={() => {}}
+          >
+            <Smile className="size-4" />
+          </Button>
+          <Button
+            disabled={false}
+            size="iconSm"
+            variant="ghost"
+            onClick={() => {}}
+          >
+            <ImageIcon className="size-4" />
+          </Button>
+          <Button
+            disabled={false}
+            onClick={() => {}}
+            size="iconSm"
+            className="ml-auto bg-[#007a5a] text-white hover:bg-[#007a5a]/80"
+          >
+            <MdSend className="size-4" />
+          </Button>
+        </div>
       </div>
     </div>
   );
