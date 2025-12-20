54554fa83ea7b4e4114b90c47b7e14cea0c15ebf
luk <luk@email.com>
2025-12-20 10:03:05 +0100

    feat: add conversations and reactions tables with appropriate indexes

--- convex/schema.ts
+++ convex/schema.ts
@@ -23,6 +23,11 @@ const schema = defineSchema({
   })
     .index("by_workspace_id", ["workspaceId"])
     .index("by_workspace_id_name", ["workspaceId", "name"]),
+  conversations: defineTable({
+    workspaceId: v.id("workspaces"),
+    memberOneId: v.id("members"),
+    memberTwoId: v.id("members"),
+  }).index("by_workspace_id", ["workspaceId"]),
   messages: defineTable({
     body: v.string(),
     image: v.optional(v.id("_storage")),
@@ -30,12 +35,28 @@ const schema = defineSchema({
     workspaceId: v.id("workspaces"),
     channelId: v.optional(v.id("channels")),
     parentMessageId: v.optional(v.id("messages")),
-    // TODO: add conversationId for DMs
+    conversationId: v.optional(v.id("conversations")),
     updatedAt: v.number(),
   })
     .index("by_workspace_id", ["workspaceId"])
+    .index("by_member_id", ["memberId"])
     .index("by_channel_id", ["channelId"])
-    .index("by_parent_message_id", ["parentMessageId"]),
+    .index("by_conversation_id", ["conversationId"])
+    .index("by_parent_message_id", ["parentMessageId"])
+    .index("by_channel_id_parent_message_id_conversation_id", [
+      "channelId",
+      "parentMessageId",
+      "conversationId",
+    ]),
+  reactions: defineTable({
+    workspaceId: v.id("workspaces"),
+    messageId: v.id("messages"),
+    memberId: v.id("members"),
+    value: v.string(),
+  })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_message_id", ["messageId"])
+    .index("by_member_id", ["memberId"]),
 });
 
 export default schema;
