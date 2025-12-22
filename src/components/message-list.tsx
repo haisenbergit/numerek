08fdbaec84774ee7e77d70a2f8609fa70a8b3007
luk <luk@email.com>
2025-12-22 15:09:27 +0100

    feat: implement Message component for rendering individual messages in MessageList

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -1,4 +1,5 @@
 import { format, isToday, isYesterday } from "date-fns";
+import { Message } from "@/components/message";
 import { GetMessagesReturnType } from "@/features/messages/api/use-get-messages";
 
 interface MessageListProps {
@@ -52,7 +53,28 @@ export const MessageList = ({
             </span>
           </div>
           {messages.map((message, index) => {
-            return <div key={message._id}>{JSON.stringify(message)}</div>;
+            return (
+              <Message
+                key={message._id}
+                id={message._id}
+                memberId={message.memberId}
+                authorImage={message.user.image}
+                authorName={message.user.name}
+                isAuthor={false}
+                reactions={message.reactions}
+                body={message.body}
+                image={message.image}
+                updatedAt={message.updatedAt}
+                createdAt={message._creationTime}
+                isEditing={false}
+                setEditingId={() => {}}
+                isCompact={false}
+                hideThreadButton={false}
+                threadCount={message.threadCount}
+                threadImage={message.threadImage}
+                threadTimestamp={message.threadTimestamp}
+              />
+            );
           })}
         </div>
       ))}
