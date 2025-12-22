073c31a2bdba692a914c1c36f541d117004d1ea8
luk <luk@email.com>
2025-12-22 20:29:31 +0100

    feat: add timestamp display for messages in Message component

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import { Doc, Id } from "@convex/_generated/dataModel";
+import { format } from "date-fns";
 
 const Renderer = dynamic(() => import("./renderer"), { ssr: false });
 
@@ -48,7 +49,12 @@ export const Message = ({
   threadTimestamp,
 }: MessageProps) => {
   return (
-    <div>
+    <div className="group relative flex flex-col gap-2 p-1.5 px-5 hover:bg-gray-100/60">
+      <div className="flex items-start gap-2">
+        <button className="w-[40px] text-center text-xs leading-[22px] text-muted-foreground opacity-0 hover:underline group-hover:opacity-100">
+          {format(new Date(createdAt), "hh:mm")}
+        </button>
+      </div>
       <Renderer value={body} />
     </div>
   );
