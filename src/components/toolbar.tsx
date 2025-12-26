5135f9c0f805f8976dd8f8b83fc6837750572211
luk <luk@email.com>
2025-12-26 09:17:41 +0100

    feat: implement message toolbar with emoji reactions, editing, and deletion options

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,3 +1,14 @@
+import {
+  MessageSquareText,
+  MessageSquareTextIcon,
+  Pencil,
+  Smile,
+  Trash,
+} from "lucide-react";
+import { EmojiPopover } from "@/components/emoji-popover";
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+
 interface ToolbarProps {
   isAuthor: boolean;
   isPending: boolean;
@@ -17,5 +28,39 @@ export const Toolbar = ({
   handleReaction,
   hideThreadButton,
 }: ToolbarProps) => {
-  return <div>Toolbar</div>;
+  return (
+    <div className="absolute right-5 top-0">
+      <div className="rounded-md border bg-white opacity-0 shadow-sm transition-opacity group-hover:opacity-100">
+        <EmojiPopover
+          hint="Add reaction"
+          onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+        >
+          <Button variant="ghost" size="iconSm" disabled={isPending}>
+            <Smile className="size-4" />
+          </Button>
+        </EmojiPopover>
+        {!hideThreadButton && (
+          <Hint label="Reply in thread">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <MessageSquareTextIcon className="size-4" />
+            </Button>
+          </Hint>
+        )}
+        {isAuthor && (
+          <Hint label="Edit message">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Pencil className="size-4" />
+            </Button>
+          </Hint>
+        )}
+        {isAuthor && (
+          <Hint label="Delete message">
+            <Button variant="ghost" size="iconSm" disabled={isPending}>
+              <Trash className="size-4" />
+            </Button>
+          </Hint>
+        )}
+      </div>
+    </div>
+  );
 };
