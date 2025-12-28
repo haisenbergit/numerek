0dd7d32d74a0092326b9ed51f6184781cc064c67
luk <luk@email.com>
2025-12-29 00:12:43 +0100

    feat: add getById query and useGetMessage hook for message retrieval in thread component

--- src/features/messages/components/thread.tsx
+++ src/features/messages/components/thread.tsx
@@ -1,6 +1,7 @@
 import { Id } from "@convex/_generated/dataModel";
-import { XIcon } from "lucide-react";
+import { AlertTriangle, Loader, XIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useGetMessage } from "@/features/messages/api/use-get-message";
 
 interface ThreadProps {
   messageId: Id<"messages">;
@@ -8,6 +9,32 @@ interface ThreadProps {
 }
 
 export const Thread = ({ messageId, onClose }: ThreadProps) => {
+  const { data: message, isLoading: loadingMessage } = useGetMessage({
+    id: messageId,
+  });
+  if (loadingMessage)
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Loader className="size-5 animate-spin text-muted-foreground" />
+      </div>
+    );
+
+  if (!message)
+    return (
+      <div className="flex h-full flex-col">
+        <div className="flex h-[49px] items-center justify-between border-b px-4">
+          <p className="text-lg font-bold">Thread</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex h-full flex-col items-center justify-center gap-y-2">
+          <AlertTriangle className="size-5 text-muted-foreground" />
+          <p className="text-sm text-muted-foreground">Message not found</p>
+        </div>
+      </div>
+    );
+
   return (
     <div className="flex h-full flex-col">
       <div className="flex h-[49px] items-center justify-between border-b px-4">
@@ -16,6 +43,7 @@ export const Thread = ({ messageId, onClose }: ThreadProps) => {
           <XIcon className="size-5 stroke-[1.5]" />
         </Button>
       </div>
+      <div>{JSON.stringify(message)}</div>
     </div>
   );
 };
