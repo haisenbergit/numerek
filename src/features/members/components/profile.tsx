8fe33884494352d2ec6a5cb5996e9597db4d1494
luk <luk@email.com>
2026-01-01 16:51:36 +0100

    feat: implement loading and error states in profile component

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,4 +1,7 @@
 import { Id } from "@convex/_generated/dataModel";
+import { AlertTriangle, Loader, XIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useGetMember } from "@/features/members/api/use-get-member";
 
 interface ProfileProps {
   memberId: Id<"members">;
@@ -6,5 +9,40 @@ interface ProfileProps {
 }
 
 export const Profile = ({ memberId, onClose }: ProfileProps) => {
+  const { data: member, isLoading: isLoadingMember } = useGetMember({
+    id: memberId,
+  });
+
+  if (isLoadingMember)
+    return (
+      <div className="flex h-full flex-col">
+        <div className="flex h-[49px] items-center justify-between border-b px-4">
+          <p className="text-lg font-bold">Profile</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex h-full flex-col items-center justify-center gap-y-2">
+          <Loader className="size-5 animate-spin text-muted-foreground" />
+        </div>
+      </div>
+    );
+
+  if (!member)
+    return (
+      <div className="flex h-full flex-col">
+        <div className="flex h-[49px] items-center justify-between border-b px-4">
+          <p className="text-lg font-bold">Profile</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex h-full flex-col items-center justify-center gap-y-2">
+          <AlertTriangle className="size-5 text-muted-foreground" />
+          <p className="text-sm text-muted-foreground">Profile not found</p>
+        </div>
+      </div>
+    );
+
   return <div>Profile</div>;
 };
