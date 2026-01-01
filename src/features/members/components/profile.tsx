6c9e2372f7cbe2d47373a62929e05801207e307d
luk <luk@email.com>
2026-01-01 17:17:39 +0100

    feat: enhance profile component with avatar and contact information display

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link";
 import { Id } from "@convex/_generated/dataModel";
-import { AlertTriangle, Loader, XIcon } from "lucide-react";
+import { AlertTriangle, Loader, MailIcon, XIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import { useGetMember } from "@/features/members/api/use-get-member";
 
 interface ProfileProps {
@@ -44,5 +47,47 @@ export const Profile = ({ memberId, onClose }: ProfileProps) => {
       </div>
     );
 
-  return <div>Profile</div>;
+  const avatarFallback = member.user.name?.charAt(0).toUpperCase() ?? "M";
+
+  return (
+    <div className="flex h-full flex-col">
+      <div className="flex h-[49px] items-center justify-between border-b px-4">
+        <p className="text-lg font-bold">Profile</p>
+        <Button onClick={onClose} size="iconSm" variant="ghost">
+          <XIcon className="size-5 stroke-[1.5]" />
+        </Button>
+      </div>
+      <div className="flex flex-col items-center justify-center p-4">
+        <Avatar className="size-full max-h-[256px] max-w-[256px]">
+          <AvatarImage src={member.user.image} />
+          <AvatarFallback className="aspect-square text-6xl">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="flex flex-col p-4">
+        <p className="tex-xl font-bold">{member.user.name}</p>
+        <Separator />
+        <div className="flex flex-col p-4">
+          <p className="mb-4 text-sm font-bold">Contact information</p>
+          <div className="flex items-center gap-2">
+            <div className="flex size-9 items-center justify-center rounded-md bg-muted">
+              <MailIcon className="size-4" />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-[13px] font-semibold text-muted-foreground">
+                Email Address
+              </p>
+              <Link
+                href={`mailto:${member.user.email}`}
+                className="text-sm text-[#1264a3] hover:underline"
+              >
+                {member.user.email}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
