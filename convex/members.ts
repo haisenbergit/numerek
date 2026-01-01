8ec2181866a931d257a1f97311b93addc5cadec7
luk <luk@email.com>
2026-01-01 21:36:45 +0100

    feat: add update and remove mutations for member management

--- convex/members.ts
+++ convex/members.ts
@@ -1,7 +1,10 @@
 import { v } from "convex/values";
 import { Id } from "./_generated/dataModel";
-import { QueryCtx, query } from "./_generated/server";
-import { getAuthenticatedUserIdForQuery } from "./utils";
+import { QueryCtx, mutation, query } from "./_generated/server";
+import {
+  getAuthenticatedUserId,
+  getAuthenticatedUserIdForQuery,
+} from "./utils";
 
 const populateUser = (ctx: QueryCtx, id: Id<"users">) => ctx.db.get(id);
 
@@ -85,5 +88,95 @@ export const current = query({
     if (!member) return null;
 
     return member;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("members"),
+    name: v.string(),
+    role: v.union(v.literal("admin"), v.literal("member")),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthenticatedUserId(ctx);
+
+    const member = await ctx.db.get(args.id);
+    if (!member) throw new Error("Member not found");
+
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .first();
+    if (!currentMember || currentMember.role !== "admin")
+      throw new Error("Not authorized");
+
+    await ctx.db.patch(args.id, {
+      role: args.role,
+    });
+    return args.id;
+  },
+});
+
+export const remove = mutation({
+  args: {
+    id: v.id("members"),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthenticatedUserId(ctx);
+
+    const member = await ctx.db.get(args.id);
+    if (!member) throw new Error("Member not found");
+
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .first();
+
+    if (!currentMember)
+      throw new Error("Not authorized to remove members from this workspace");
+
+    if (member.role === "admin")
+      throw new Error("Cannot remove self admin member");
+
+    if (currentMember._id === args.id && currentMember.role === "admin")
+      throw new Error("Cannot remove self if self is admin");
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query("messages")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("reactions")
+        .withIndex("by_member_id", (q) => q.eq("memberId", member._id))
+        .collect(),
+      ctx.db
+        .query("conversations")
+        .filter((q) =>
+          q.or(
+            q.eq(q.field("memberOneId"), member._id),
+            q.eq(q.field("memberTwoId"), member._id)
+          )
+        )
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id);
+    }
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id);
+    }
+
+    await ctx.db.delete(args.id);
+    return args.id;
   },
 });
