3b66687e31752b646f61ac356f780f5f98c436f9
luk <luk@email.com>
2026-01-01 23:26:09 +0100

    feat: add confirmation dialogs for member role updates, removals, and leaving workspace

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -7,6 +7,7 @@ import {
   MailIcon,
   XIcon,
 } from "lucide-react";
+import { toast } from "sonner";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -14,6 +15,7 @@ import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetMember } from "@/features/members/api/use-get-member";
 import { useRemoveMember } from "@/features/members/api/use-remove-member";
 import { useUpdateMember } from "@/features/members/api/use-update-member";
+import { useConfirmationWindow } from "@/hooks/use-confirmation-window";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
 interface ProfileProps {
@@ -23,6 +25,21 @@ interface ProfileProps {
 
 export const Profile = ({ memberId, onClose }: ProfileProps) => {
   const workspaceId = useWorkspaceId();
+
+  const [UpdateDialog, confirmUpdate] = useConfirmationWindow(
+    "Change role",
+    "Are you sure you want to change this member's role?"
+  );
+
+  const [LeaveDialog, confirmLeave] = useConfirmationWindow(
+    "Leave workspace",
+    "Are you sure you want to leave this workspace?"
+  );
+
+  const [RemoveDialog, confirmRemove] = useConfirmationWindow(
+    "Remove member",
+    "Are you sure you want to remove this member?"
+  );
 
   const { data: member, isLoading: isLoadingMember } = useGetMember({
     id: memberId,
@@ -36,6 +53,60 @@ export const Profile = ({ memberId, onClose }: ProfileProps) => {
     useUpdateMember();
   const { mutate: removeMember, isPending: isRemovingMember } =
     useRemoveMember();
+
+  const onRemove = async () => {
+    const isConfirmed = await confirmRemove();
+    if (!isConfirmed) return;
+
+    removeMember(
+      { id: memberId },
+      {
+        onSuccess: () => {
+          toast.success("Member removed");
+          onClose();
+        },
+        onError: () => {
+          toast.error("Failed to remove member");
+        },
+      }
+    );
+  };
+
+  const onLeave = async () => {
+    const isConfirmed = await confirmLeave();
+    if (!isConfirmed) return;
+
+    removeMember(
+      { id: memberId },
+      {
+        onSuccess: () => {
+          toast.success("You have left the workspace");
+          onClose();
+        },
+        onError: () => {
+          toast.error("Failed to leave the workspace");
+        },
+      }
+    );
+  };
+
+  const onUpdate = async (role: "admin" | "member") => {
+    const isConfirmed = await confirmUpdate();
+    if (!isConfirmed) return;
+
+    updateMember(
+      { id: memberId, role },
+      {
+        onSuccess: () => {
+          toast.success("Role changed");
+          onClose();
+        },
+        onError: () => {
+          toast.error("Failed to change role");
+        },
+      }
+    );
+  };
 
   if (isLoadingMember || isLoadingCurrentMember)
     return (
@@ -71,61 +142,67 @@ export const Profile = ({ memberId, onClose }: ProfileProps) => {
   const avatarFallback = member.user.name?.charAt(0).toUpperCase() ?? "M";
 
   return (
-    <div className="flex h-full flex-col">
-      <div className="flex h-[49px] items-center justify-between border-b px-4">
-        <p className="text-lg font-bold">Profile</p>
-        <Button onClick={onClose} size="iconSm" variant="ghost">
-          <XIcon className="size-5 stroke-[1.5]" />
-        </Button>
-      </div>
-      <div className="flex flex-col items-center justify-center p-4">
-        <Avatar className="size-full max-h-[256px] max-w-[256px]">
-          <AvatarImage src={member.user.image} />
-          <AvatarFallback className="aspect-square text-6xl">
-            {avatarFallback}
-          </AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="flex flex-col p-4">
-        <p className="text-xl font-bold">{member.user.name}</p>
-        {currentMember?.role === "admin" && currentMember?._id !== memberId ? (
-          <div className="mt-4 flex items-center gap-2">
-            <Button variant="outline" className="w-full capitalize">
-              {member.role} <ChevronDownIcon className="ml-2 size-4" />
-            </Button>
-            <Button variant="outline" className="w-full">
-              Remove
-            </Button>
-          </div>
-        ) : currentMember?._id === memberId &&
-          currentMember?.role !== "admin" ? (
-          <div className="mt-4">
-            <Button variant="outline" className="w-full">
-              Leave
-            </Button>
-          </div>
-        ) : null}
-      </div>
-      <Separator />
-      <div className="flex flex-col p-4">
-        <p className="mb-4 text-sm font-bold">Contact information</p>
-        <div className="flex items-center gap-2">
-          <div className="flex size-9 items-center justify-center rounded-md bg-muted">
-            <MailIcon className="size-4" />
-          </div>
-          <div className="flex flex-col">
-            <p className="text-[13px] font-semibold text-muted-foreground">
-              Email Address
-            </p>
-            <Link
-              href={`mailto:${member.user.email}`}
-              className="text-sm text-[#1264a3] hover:underline"
-            >
-              {member.user.email}
-            </Link>
+    <>
+      <RemoveDialog />
+      <LeaveDialog />
+      <UpdateDialog />
+      <div className="flex h-full flex-col">
+        <div className="flex h-[49px] items-center justify-between border-b px-4">
+          <p className="text-lg font-bold">Profile</p>
+          <Button onClick={onClose} size="iconSm" variant="ghost">
+            <XIcon className="size-5 stroke-[1.5]" />
+          </Button>
+        </div>
+        <div className="flex flex-col items-center justify-center p-4">
+          <Avatar className="size-full max-h-[256px] max-w-[256px]">
+            <AvatarImage src={member.user.image} />
+            <AvatarFallback className="aspect-square text-6xl">
+              {avatarFallback}
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="flex flex-col p-4">
+          <p className="text-xl font-bold">{member.user.name}</p>
+          {currentMember?.role === "admin" &&
+          currentMember?._id !== memberId ? (
+            <div className="mt-4 flex items-center gap-2">
+              <Button variant="outline" className="w-full capitalize">
+                {member.role} <ChevronDownIcon className="ml-2 size-4" />
+              </Button>
+              <Button variant="outline" className="w-full">
+                Remove
+              </Button>
+            </div>
+          ) : currentMember?._id === memberId &&
+            currentMember?.role !== "admin" ? (
+            <div className="mt-4">
+              <Button variant="outline" className="w-full">
+                Leave
+              </Button>
+            </div>
+          ) : null}
+        </div>
+        <Separator />
+        <div className="flex flex-col p-4">
+          <p className="mb-4 text-sm font-bold">Contact information</p>
+          <div className="flex items-center gap-2">
+            <div className="flex size-9 items-center justify-center rounded-md bg-muted">
+              <MailIcon className="size-4" />
+            </div>
+            <div className="flex flex-col">
+              <p className="text-[13px] font-semibold text-muted-foreground">
+                Email Address
+              </p>
+              <Link
+                href={`mailto:${member.user.email}`}
+                className="text-sm text-[#1264a3] hover:underline"
+              >
+                {member.user.email}
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
